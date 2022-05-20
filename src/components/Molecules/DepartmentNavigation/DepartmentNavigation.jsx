ea5193108068202f1149ad5b70828ff9a21713a7
viver <viverbungag1@gmail.com>
2022-05-21 01:36:57 +0800

    Deleted src

--- src/components/Molecules/DepartmentNavigation/DepartmentNavigation.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DepartmentNavigation = () => {
-  return (
-    <div className="departmentNavigationContainer">DepartmentNavigation</div>
-  );
-};
-
-export default DepartmentNavigation;
